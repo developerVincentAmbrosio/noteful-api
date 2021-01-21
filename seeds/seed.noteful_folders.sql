98d42129f5b0e8e01aa2ddb49c9e093a2a2a3032
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-01-21 17:12:04 -0500

    wrote create undo and seed  sql files

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_folders (id, name)
+VALUES
+('b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1','Important'),
+('b07161a6-ffaf-11e8-8eb2-f2801f1b9fd2','Super'),
+('b0715efe-ffaf-11e8-8eb2-f2801f1b9fd3','Spangley');
