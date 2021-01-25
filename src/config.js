ca38e341c68ac87b5d92058769459b903283462a
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-01-25 16:37:45 -0500

    changed config file

--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || '',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:postgres@127.0.0.1/noteful',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres:postgres@127.0.0.1/noteful-test'
   }
